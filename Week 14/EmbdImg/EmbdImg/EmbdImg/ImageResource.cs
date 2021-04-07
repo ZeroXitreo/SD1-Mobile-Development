3920279829569b4e322620cfbcb6030bf28b433e
Devulak <Devulak@users.noreply.github.com>
2021-04-07 10:45:46 +0200

    f

---
+++ Week 14/EmbdImg/EmbdImg/EmbdImg/ImageResource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EmbdImg
+{
+    [ContentProperty(nameof(Source))]
+    class ImageResource : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (!string.IsNullOrEmpty(Source))
+            {
+                return ImageSource.FromResource(Source, typeof(ImageResource).GetTypeInfo().Assembly);
+            }
+
+            return null;
+        }
+    }
+}
