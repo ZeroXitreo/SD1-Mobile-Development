7c357845f0864c37deb90e602b1c5b5dd765de80
Devulak <Devulak@users.noreply.github.com>
2021-04-21 21:36:42 +0200

    brr

---
+++ Week 16/CamTest/CamTest/CamTest/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CamTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, true);
+            Title = "CamTest boi";
+        }
+
+        private async void BtnCam_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions()
+                {
+                    DefaultCamera = CameraDevice.Rear,
+                    Directory = "Xamarin",
+                    SaveToAlbum = true
+                });
+
+                if (photo != null)
+                    imgCam.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, ex.Message.ToString(), "Ok");
+                throw;
+            }
+        }
+    }
+}
